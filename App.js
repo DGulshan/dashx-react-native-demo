b6e1bd094c5f6dd4e2a396be845eee88cdc5d472
Gulshan Dhingra <gdgulshan.007@gmail.com>
2022-01-22 14:07:31 +0530

    Merged commits into one

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
+import Config from 'react-native-config';
+import DashX from '@dashx/react-native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+
+DashX.setLogLevel(0);
+
+DashX.setup({
+  publicKey: Config.DASHX_PUBLIC_KEY,
+  trackAppLifecycleEvents: false,
+  trackScreenViews: false,
+  targetEnvironment: Config.DASHX_TARGET_ENVIRONMENT || 'production'
+});
 
 export default function App() {
+  useEffect(() => {
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
